7ab47571ce60405c7be189950799125290bb0ef6
davin <davinzhang@outlook.com>
2019-11-12 07:48:35 +0800

    fixed

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/essential'
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'vue'
+    ],
+    'rules': {
+            "parser": "babel-eslint",
+        'no-unused-vars': 0,
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        "ecmaFeatures": {
+            "vue": true,
+            "experimentalObjectRestSpread": true
+        },
+        "parserOptions": {
+            "sourceType": "module"
+        },
+        'no-console': 'off'
+    }
+}
